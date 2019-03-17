ee2feae2c8a21472f932a95552bfe15115fc77b6
Tony Jojan <tony.jojan@gmail.com>
2019-03-17 15:02:56 -0700

    added action creator

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
 import { Header } from './components/common';
+import LibraryList from './components/LibraryList';
 
 
 const App = () => {
   return (
     <Provider store={createStore(reducers)}>
-      <View>
-        <Header headerText="Tech Stack" .>
+      <View style={{ flex:1 }}>
+        <Header headerText="Tech Stack" />
+        <LibraryList />
       </View>
     </Provider>
   );
--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
+import { FlatList } from 'react-native';
+import { connect } from 'react-redux';
+import ListItem from './ListItem';
 
 class LibraryList extends Component {
+  renderItem(library){
+    return <ListItem library={library} />;
+  }
   render(){
-    return;
+    return(
+      <FlatList
+        data={this.props.libraries}
+        renderItem={this.renderItem}
+        keyExtractor={library => library.id.toString()}
+      />
+    );
   }
 }
 
-export default LibraryList;
+const mapStateToProps = state => {
+  return { libraries: state.libraries };
+}
+export default connect(mapStateToProps)(LibraryList);
